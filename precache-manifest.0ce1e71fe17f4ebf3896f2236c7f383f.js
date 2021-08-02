d5ef47b5155d7e0f8e325c9b9e00d5858715f9a8
Ramya Narayanan <ramyanaryn@gmail.com>
2021-08-02 15:47:47 +0800

    Updates

---
+++ precache-manifest.0ce1e71fe17f4ebf3896f2236c7f383f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5471bc2188ef1898a689698718ba351d",
+    "url": "/Grocery-List/index.html"
+  },
+  {
+    "revision": "ed3e664ed54991bd72b5",
+    "url": "/Grocery-List/static/css/main.3862cf09.chunk.css"
+  },
+  {
+    "revision": "ea1b597a0d05135a845c",
+    "url": "/Grocery-List/static/js/2.9bcf8ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery-List/static/js/2.9bcf8ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3e664ed54991bd72b5",
+    "url": "/Grocery-List/static/js/main.7be6418e.chunk.js"
+  },
+  {
+    "revision": "95c205694fd8ecbc80df",
+    "url": "/Grocery-List/static/js/runtime-main.472b5073.js"
+  }
+]);
